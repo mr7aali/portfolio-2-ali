f8c79a9f3cb46773eb4939bf816942f1c8c2f770
mr7aali <mr7aali@gmail.com>
2023-03-26 19:34:05 +0600

    center the dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,21 @@ import Navbar from "../../components/Navbar";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
-        <div className="col-span-12 p-4 text-center bg-white dark:bg-dark-500  lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
-          <SideBar />
-        </div>
-        <div className="flex flex-col col-span-12 bg-white dark:bg-dark-500  lg:col-span-9 rounded-2xl overflow-hidden shadow-custom-light dark:shadow-custom-dark">
-          <Navbar />
-          <Component {...pageProps} />
+      <div style={{
+        border:'1px solid red',
+        minHeight:'100vh',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center'
+      }}>
+        <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32">
+          <div className="col-span-12 p-4 text-center bg-white dark:bg-dark-500  lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
+            <SideBar />
+          </div>
+          <div className="flex flex-col col-span-12 bg-white dark:bg-dark-500  lg:col-span-9 rounded-2xl overflow-hidden shadow-custom-light dark:shadow-custom-dark">
+            <Navbar />
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </ThemeProvider>
