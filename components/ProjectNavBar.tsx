15ac5c2c47c6426c5a0a2bfb675356b0d7a1fcaa
mr7aali <mr7aali@gmail.com>
2023-03-31 22:22:55 +0600

    FilterCategory works

--- components/ProjectNavBar.tsx
+++ components/ProjectNavBar.tsx
@@ -1,26 +1,35 @@
-import { FunctionComponent } from 'react'
-import { Category } from 'type';
+import { FunctionComponent } from "react";
+import { Category } from "type";
 
-// export  function NavItem: FunctionComponent<{value:Category | 'all'}>=({value})=> {
-//   return (
-//     <li>{value}</li>
-//   )
-// }
+const NavItem: FunctionComponent<{
+  value: Category | "all";
+  FilterCategory: Function;
+  active: string;
+}> = ({ value, FilterCategory, active }) => {
+  let activeClassName = "capitalize cursor-pointer hover:text-green";
+  if ( active === value) {
+    activeClassName = activeClassName + " text-green";
+  }
+  return (
+    <li className={activeClassName} onClick={() => FilterCategory(value)}>
+      {value}
+    </li>
+  );
+};
 
-const NavItem:FunctionComponent<{
-    value: Category | 'all'
-}>=({value})=>{
-    return(
-        <li>{value}</li>
-    );
-}
-
-const ProjectNavBar = () => {
-    return (
-        <div>
-            <NavItem value={'all'}/>
-        </div>
-    );
+const ProjectNavBar: FunctionComponent<{
+  FilterCategory: Function;
+  active: string;
+}> = ({ FilterCategory, active }) => {
+  return (
+    <div className="flex px-3 space-x-3 overflow-x-auto list-none">
+      <NavItem value="all" FilterCategory={FilterCategory} active={active} />
+      <NavItem value="react" FilterCategory={FilterCategory} active={active} />
+      <NavItem value="mongo" FilterCategory={FilterCategory} active={active} />
+      <NavItem value="django" FilterCategory={FilterCategory} active={active} />
+      <NavItem value="node" FilterCategory={FilterCategory} active={active} />
+    </div>
+  );
 };
 
 export default ProjectNavBar;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,36 @@
 import ProjectCart from "components/ProjectCart";
-import { projects } from "data";
+import ProjectNavBar from "components/ProjectNavBar";
+import { projects as projectsData } from "data";
+import { useState } from "react";
+import { Category } from "type";
 
 const Project = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
+
+  const FilterCategory = (category: Category | "all") => {
+    if (category === "all") {
+      setProjects(projectsData);
+      setActive(category);
+      console.log("all filter");
+    } else {
+      const newProjects = projectsData.filter((p) => p.category.includes(category));
+      setProjects(newProjects);
+      setActive(category);
+      console.log("category filter");
+    }
+  };
+
   return (
-    <div className="px-5 py-2 overflow-y-scroll" style={{height:'60vh'}}>
-      <nav>Project</nav>
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "60vh" }}>
+      <ProjectNavBar FilterCategory={FilterCategory} active={active}/>
       <div className="grid grid-cols-12 gap-4 my-3 relative">
         {projects.map((project, i) => (
-          <div key={project.name} className='col-span-12 sm:col-span-6 lg:col-span-4 p-2 bg-gray-200 dark:bg-dark-200 rounded-lg'>
-            <ProjectCart project={project}  />
+          <div
+            key={project.name}
+            className="col-span-12 sm:col-span-6 lg:col-span-4 p-2 bg-gray-200 dark:bg-dark-200 rounded-lg"
+          >
+            <ProjectCart project={project} />
           </div>
         ))}
       </div>
